162afb8ca4167f9d4db202b4efe91acc357f83ec
nicolasGuinazu <nicolasgarcia796@gmail.com>
2021-06-11 18:13:57 -0300

    correccion error para la ruta del register

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,5 @@ var indexController=require('../Controllers/indexController')
 
 router.get('/', indexController.index);
 router.get('/register', indexController.register);
-router.get('/login', indexController.register);
+router.get('/login', indexController.login);
 module.exports = router;
